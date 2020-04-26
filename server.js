0859a89b11555c85888fcfa3338d8be4a7d5034a
yogamahottama <yoga.mahottama@gmail.com>
2020-04-26 09:40:40 +0800

    indikator satker

--- server.js
+++ server.js
@@ -499,11 +499,63 @@ app.delete("/api/capaiam-unit/:id&:id2", function(req, res)
    executeQuery(re, query, model, 1)
 })
 
-//Log Indikator Satuan Kerja
+///////////////////////\\\\\\\\\\\\\\\\\\\\\\\
+///////////////Indikator Satuan Kerja\\\\\\\\\\
 
-app.get("/api/logindikatorsatker", function(req, res){
-  var query = "select * from Indikator_SatuanKerja_Log"
-  executeQuery(res, query, null, 0)
+//Select
+app.get("/api/indikator-satuankerja/", function(req, res)
+{
+   var query = "select * from Indikator_SatuanKerja"
+   executeQuery(res, query, null, 0)
+})
+
+//Insert
+app.post("/api/indikator-satuankerja/", function(req, res)
+{
+   var model = [
+      { name: 'id_periode', sqltype: sql.Numeric, value: req.body.id_periode },
+      { name: 'id_master', sqltype: sql.Int, value: req.body.id_master },
+      { name: 'id_satker', sqltype: sql.UniqueIdentifier, value: req.body.id_satker },
+      { name: 'bobot', sqltype: sql.Float, value: req.body.bobot },
+      { name: 'target', sqltype: sql.Float, value: req.body.target },
+      { name: 'capaian', sqltype: sql.Float, value: req.body.capaian }
+   ]
+
+   var query = "insert into Indikator_SatuanKerja values( @id_periode, @id_master, @id_satker, @bobot, @target, @capaian, CURRENT_TIMESTAMP"
+   executeQuery(res, query, model, 1)
+})
+
+//Update
+app.put("/api/indikator-satuankerja/:id&:id2&:id3", function(req, res)
+{
+   var model = [
+      { name: 'id_periode', sqltype: sql.Numeric, value: req.body.id_periode },
+      { name: 'id_master', sqltype: sql.Int, value: req.body.id_master },
+      { name: 'id_satker', sqltype: sql.UniqueIdentifier, value: req.body.id_satker },
+      { name: 'bobot', sqltype: sql.Float, value: req.body.bobot },
+      { name: 'target', sqltype: sql.Float, value: req.body.target },
+      { name: 'capaian', sqltype: sql.Float, value: req.body.capaian },
+      { name: 'id', sqltype: sql.Numeric, value: req.params.id },
+      { name: 'id2', sqltype: sql.Int, value: req.params.id2 },
+      { name: 'id3', sqltype: sql.UniqueIdentifier, value: req.params.id3 }
+  ]
+
+   var query = "update Indikator_SatuanKerja set id_periode = @id_periode, id_master = @id_master, id_satker = @id_satker, bobot = @bobot, targer = @target " +
+               "capaian = @capaian, last_update = CURRENT_TIMESTAMP where id_periode = @id and id_master = @id2 and id_satker = @id3"
+   executeQuery(res, query, model, 1)
+})
+
+//Delete
+app.delete("/api/indikator-satuankerja/:id&:id2&:id3", function(req, res)
+{
+   var model = [
+      { name: 'id_periode', sqltype: sql.Numeric, value: req.params.id },
+      { name: 'id_master', sqltype: sql.Int, value: req.params.id2 },
+      { name: 'id_satker', sqltype: sql.UniqueIdentifier, value: req.params.id3 }
+   ]
+
+   var query = "delete from Indikator_SatuanKerja where id_periode = @id_periode and id_master = @id_master and id_satker = @id_satker"
+   executeQuery(res, query, model, 1)
 })
 
 //  LISTEN
